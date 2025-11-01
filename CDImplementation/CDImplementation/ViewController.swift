3e1f6a007398a0c0d8ae001e398a1f049a39903b
Swamy Vemulawada <1773386_swamyios@titan.co.in>
2025-11-01 22:13:54 +0530

    error

--- CDImplementation/CDImplementation/ViewController.swift
+++ CDImplementation/CDImplementation/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var temp:Int?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        temp  = ""
     }
 
 
